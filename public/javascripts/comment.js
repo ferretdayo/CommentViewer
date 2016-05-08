e2d190951fea2fadb90e00b8cf25e3340f054ccc
ferretdayo <ferretdayo@hotmail.co.jp>
2016-05-08 15:03:21 +0900

    放送URLからコメント取得＋socket.ioでViewに描画完了(disconnect処理はまだ)

--- public/javascripts/comment.js
+++ public/javascripts/comment.js
@@ -1,4 +1,16 @@
-var socket = io();
+var socket = io.connect('http://localhost:3001');
+//var socket = new io.Socket('localhost');
+socket.connect();
 socket.on('comment data', function(msg){
-    $('#comment').append($('<tr>'+ msg+'</tr>'));
+    if(msg.hasOwnProperty('chat')){
+        console.table(msg.chat);
+        if(msg.chat.$.hasOwnProperty('premium')){
+            $('#comment').append($('<tr><td>' + msg.chat.$.user_id + '</td><td>' + msg.chat._ + '</td><td>' + msg.chat.$.premium + '</td></tr>'));
+        }else{
+            $('#comment').append($('<tr><td>' + msg.chat.$.user_id + '</td><td>' + msg.chat._ + '</td><td>一般会員</td></tr>'));
+        }   
+    }
 });
+socket.on('disconnect', function(){
+    console.log("放送終わりました");
+})
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var nicolive = require('../nicolive.js');
+var parser = require('xml2js').parseString;
 
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var preSocket = -1;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'NicoNicoCommentViewer' });
@@ -15,19 +20,40 @@ router.post('/', function(req, res, next){
     
     var user_session = req.cookies.user_session;
     
-    nicolive.fetchThread(lv,user_session,function(error,thread){
-        if(error!=null) throw error;
-        nicolive.view(thread,function(error,viewer){
+    //io.sockets[preSocket].disconnect();
+    
+    io.on('connection', function(socket){
+        preSocket = socket.id;
+        console.log(preSocket);
+        nicolive.fetchThread(lv,user_session,function(error,thread){
             if(error!=null) throw error;
-            viewer.on('data',function(data){
-                //Socket.ioでクライアント側にコメント来たらemitしたい・・・
-                //まずsocketioとelectron共存できるんかいな・・・
-                
-                console.log(data);
+            nicolive.view(thread,function(error,viewer){
+                if(error!=null) throw error;
+                viewer.on('data',function(data){
+                    console.log(data+"\n");
+                    //Socket.ioでクライアント側にコメント来たらemitしたい・・・
+                    //まずsocketioとelectron共存できるんかいな・・・
+                    parser(data, function(err, result){
+                        socket.emit('comment data', result);
+                    });
+                });
             });
         });
     });
     res.render('index', { title: 'NicoNicoCommentViewer' });
+});
+
+//
+//io.on('connection', function(socket){
+//    console.log("connection");
+//    socket.on('comment data', function(msg){
+//        console.log('message: ' + msg);
+//    });
+//});
+
+
+http.listen(3001, function(){
+  console.log('listening on *:3000');
 });
 
 module.exports = router;

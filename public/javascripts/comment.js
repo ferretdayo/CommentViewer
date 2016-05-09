1cfd6b6130cbdc367e1ac27b3fd421713ad40340
ferretdayo <ferretdayo@hotmail.co.jp>
2016-05-09 22:36:21 +0900

    データのアナライズ関数追加(まだ未完)

--- public/javascripts/comment.js
+++ public/javascripts/comment.js
@@ -2,18 +2,56 @@ var socket = io.connect('http://localhost:3001');
 //var socket = new io.Socket('localhost');
 socket.connect();
 socket.on('comment data', function(msg){
-    //コメントデータの時
-    if(msg.hasOwnProperty('chat')){
-        console.table(msg.chat);
-        //プレミアムの時
-        if(msg.chat.$.hasOwnProperty('premium')){
-            $('#comment').prepend($('<tr><td>' + msg.chat.$.user_id + '</td><td>' + msg.chat._ + '</td><td>' + msg.chat.$.premium + '</td></tr>'));
-        //一般会員の時
-        }else{
-            $('#comment').prepend($('<tr><td>' + msg.chat.$.user_id + '</td><td>' + msg.chat._ + '</td><td>一般会員</td></tr>'));
-        }   
+    //コメントデータを形成
+    var commentData = dataAnalyze(msg);
+    if(commentData != -1){
+        $('#comment').prepend($('<tr><td>' + commentData.user_id + '</td><td>' + commentData.comment + '</td><td>' + commentData.premium + '</td></tr>'));
     }
 });
 socket.on('disconnect', function(){
     console.log("放送終わりました");
 })
+
+//コメントデータから表示する為のデータを形成する。
+function dataAnalyze(data){
+    var commentData = {};
+    //コメント情報の場合
+    if(data.hasOwnProperty('chat')){
+        console.table(data);
+        //ユーザIDの取得
+        commentData.user_id = data.chat.$.user_id;
+        //ユーザの種別
+        if(data.chat.$.hasOwnProperty('premium')){
+            switch(data.chat.$.premium){
+                case "1":
+                    commentData.premium = "プレミア";
+                    break;
+                case "3":
+                    commentData.premium = "運営";
+                    break;
+                default:
+                    console.log(data.chat.$.premium);
+                    break;
+            }
+        }else{
+            commentData.premium = "一般";
+        }
+        //コメントの取得
+        //TODO /hbから始まるときは表示しないようにする
+        commentData.comment = data.chat._;
+        //コメントの番号
+        commentData.no = data.chat.$.no;
+        //コメントの日時
+        commentData.date = createDateJST(data.chat.$.date);
+        return commentData;        
+    }else{
+        console.table(data);
+        return -1;
+    }
+}
+    
+//形成された日時を返却する関数
+function createDateJST(timestamp){
+    var d = new Date(parseInt(timestamp)*1000);
+    return d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+}

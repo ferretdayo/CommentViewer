fe88e2dc43705bf697977d6bf57800b728eca0ad
ferretdayo <ferretdayo@hotmail.co.jp>
2016-06-15 10:20:02 +0900

    add release.js

---
+++ release.js
@@ -0,0 +1,34 @@
+var packager = require('electron-packager');
+var config = require('./package.json');
+ 
+packager({
+    dir: './',
+    out: '../dist',
+    name: config.name,
+    platform: 'win32,darwin',
+    arch: 'x64',
+    version: '0.37.7',
+     
+    'app-bundle-id': 'io.github.ferretdayo', //<- ¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?
+ 
+    'app-version': config.version,
+    'helper-bundle-id': 'io.github.ferretdayo', //<- ¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?
+    overwrite: true,
+    asar: true,
+    prune: true,
+    ignore: "node_modules/(electron-packager|electron-prebuilt|\.bin)|release\.js",
+    'version-string': {
+        CompanyName: 'ferretdayo',
+        FileDescription: 'This is CommentViewer in NicoNico Broadcast',
+        OriginalFilename: config.name,
+        FileVersion: config.version,
+        ProductVersion: config.version,
+        ProductName: config.name,
+        InternalName: config.name
+    }
+}, function done (err, appPath) {
+    if(err) {
+        throw new Error(err);
+    }
+    console.log('Done!!');
+});
